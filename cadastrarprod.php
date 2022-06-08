123c3f864feae4b938e6ac249ee3b09e88c37d34
Juninho <100362679+juninhosouzaof@users.noreply.github.com>
2022-06-07 21:31:26 -0300

    Início do projeto
    Criação do menu e função cadastrar

---
+++ cadastrarprod.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Loja de Roupas - Cadastro de Produtos</title>
+</head>
+
+<?php
+
+include('menu.php')
+
+?>
+
+<header>
+    <h1>Loja de Roupas - Cadastro de Produtos</h1>
+</header>
+
+
+
+
+
+</body>
+
+</html>
---
+++ menu.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Loja de Roupas</title>
+</head>
+
+<body>
+
+    <nav>
+        <ul>
+
+            <li>Início</li>
+            <li>Tarefas</li>
+            <details>
+                <summary>
+                    Estoque
+                </summary>
+                <ul>
+                    <li><a href="#">Cadastro de produtos</a></li>
+                    <li><a href="#">Visão geral do estoque</a></li>
+                    <li><a href="#">Pesquisa refinada</a></li>
+                </ul>
+            </details>
+
+            <details>
+                <summary>
+                    Funcionários
+                </summary>
+                <ul>
+                    <li><a href="#">Cadastro de funcionários</a></li>
+                    <li><a href="#">Quadro de funcionários</a></li>
+                    <li><a href="#">Pesquisa refinada</a></li>
+                </ul>
+            </details>
+
+        </ul>
+    </nav>
+
+
+</body>
+
+</html>
