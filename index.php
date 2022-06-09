2b430f8c07d5272792cb471f613802051a5100bb
Juninho <100362679+juninhosouzaof@users.noreply.github.com>
2022-06-09 00:27:26 -0300

    Atualização bônus
    Adição de opções como acesso rápido na home page do sistema, além do próprio menu

--- index.php
+++ index.php
@@ -14,13 +14,47 @@
 include('menu.php');
 ?>
 
+        <!--TÍTULO DA PÁGINA-->
+        <header>
+            <h1>Loja de Roupas - Home</h1>
+        </header>
+        <hr>
+        <br>
 
-    <!--RODAPÉ-->
-    <br>
-    <hr>
-    <footer>
-        <p>UC MS Project - Curso Técnico em Informática - SENAC 2022</p>
-    </footer>
+
+        <!--CONTEÚDO DA PÁGINA - TABELA DE OPÇÕES-->
+
+        <table border='1'>
+
+            <tr>
+                <legend>Estoque</legend>
+                <td><a href="cadastrarprod.php">Cadastro de produtos</a></td>
+                <td><a href="visaogeralprod.php">Visão geral do estoque</a></td>
+                <td><a href="buscaprod.php">Pesquisa refinada de produtos</a></td>
+            </tr>
+        </table>
+
+        <br>
+
+        <table border='1'>
+            <legend>
+                Funcionários
+            </legend>
+            <tr>
+                <td><a href="cadastrarfunc.php">Cadastro de funcionários</a></td>
+                <td><a href="visaogeralfunc.php">Quadro de funcionários</a></td>
+                <td><a href="buscafunc.php">Pesquisa refinada de funcionários</a></td>
+            </tr>
+
+        </table>
+
+
+        <!--RODAPÉ-->
+        <br>
+        <hr>
+        <footer>
+            <p>UC MS Project - Curso Técnico em Informática - SENAC 2022</p>
+        </footer>
 </body>
 
 </html>
